85363823b46cd767eff322b6415e67ee02b6bfad
Cashmire <mzimhle.mosiwe@gmail.com>
2026-05-26 14:45:08 +0200

    Fix form fields and allow post submissions

--- app/src/Books/templates/create.php
+++ app/src/Books/templates/create.php
@@ -3,27 +3,32 @@
  * @var array $authors
  */
 ?>
-<form method="get" action="">
+<form method="POST" action="/books/create">
     <table>
         <tr>
             <td>Author</td>
             <td>
-                <select name="author_id">
-                    <?php foreach ($authors as $author): ?>
-                        <option value="<?= $author->id ?>"><?= $author->first_name ?> <?= $author->last_name ?></option>
-                    <?php endforeach; ?>
+                <select name="author_id" id="author_id">
+                    <?php if(isset($authors) && count($authors) > 0) { ?>
+                        <?php foreach ($authors as $author) { ?>
+                            <option value="<?= $author->id ?>"><?= $author->first_name ?> <?= $author->last_name ?></option>
+                        <?php } 
+                        } else {
+                            ?>
+                            <option value="">No authors found, please create an author first</option>
+                         <?php } ?>
                 </select>
             </td>
         </tr>
 
         <tr>
             <td>Title</td>
-            <td><input type="text" name="title" /> </td>
+            <td><input type="text" name="title" id="title" value="" /> </td>
         </tr>
 
         <tr>
             <td>Price (ZAR)</td>
-            <td><input type="text" name="price[ZAR]" /></td>
+            <td><input type="text" name="price[ZAR]" id="price[ZAR]" value="" /></td>
         </tr>
 
         <tr>
